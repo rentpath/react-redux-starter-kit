b185ac3522453f36d0f9eb83b9a1bc95deca76c0
Caleb Wright <awright@rentpath.com>
2015-10-26 15:32:48 -0400

    Legit component test

--- src/components/Books/__tests__/SearchStatus-test.js
+++ src/components/Books/__tests__/SearchStatus-test.js
@@ -1,8 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
 import { expect } from 'chai';
 import SearchStatus from '../SearchStatus';
 
 describe('SearchStatus', () => {
-  it('is', () => {
-    expect(SearchStatus).to.equal(SearchStatus);
+  describe('render', () => {
+    describe('when it is fetching', () => {
+      let component;
+      let node;
+
+      before('mount the component', () => {
+        component = TestUtils.renderIntoDocument(<SearchStatus isFetching didInvalidate />);
+        node = ReactDOM.findDOMNode(component);
+      });
+
+      it('assigns the right class name', () => {
+        expect(node.className).to.include('waiting');
+      });
+
+      it('renders the text', () => {
+        expect(node.textContent).to.equal('Searching...');
+      });
+    });
   });
 });
