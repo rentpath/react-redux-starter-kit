469b481de9c34b71ce96713db0a6454b1981aad0
Caleb Wright <awright@rentpath.com>
2015-10-26 16:48:50 -0400

    Expand SearchStatus tests

--- src/components/Books/__tests__/SearchStatus-test.js
+++ src/components/Books/__tests__/SearchStatus-test.js
@@ -11,7 +11,7 @@ describe('SearchStatus', () => {
       let node;
 
       before('mount the component', () => {
-        component = TestUtils.renderIntoDocument(<SearchStatus isFetching didInvalidate />);
+        component = TestUtils.renderIntoDocument(<SearchStatus isFetching />);
         node = ReactDOM.findDOMNode(component);
       });
 
@@ -21,6 +21,42 @@ describe('SearchStatus', () => {
 
       it('renders the text', () => {
         expect(node.textContent).to.equal('Searching...');
+      });
+    });
+
+    describe('when it is invalidated', () => {
+      let component;
+      let node;
+
+      before('mount the component', () => {
+        component = TestUtils.renderIntoDocument(<SearchStatus didInvalidate />);
+        node = ReactDOM.findDOMNode(component);
+      });
+
+      it('assigns the right class name', () => {
+        expect(node.className).to.include('uhoh');
+      });
+
+      it('renders the text', () => {
+        expect(node.textContent).to.equal('Uh oh! Something went wrong.');
+      });
+    });
+
+    describe('when it is neither fetching nor invalidated', () => {
+      let component;
+      let node;
+
+      before('mount the component', () => {
+        component = TestUtils.renderIntoDocument(<SearchStatus />);
+        node = ReactDOM.findDOMNode(component);
+      });
+
+      it('does not assign a class name', () => {
+        expect(node.className).to.equal('');
+      });
+
+      it('renders empty text', () => {
+        expect(node.textContent).to.equal('');
       });
     });
   });
